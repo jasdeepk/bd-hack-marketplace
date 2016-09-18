7ea9f5d4cb753f94adb15080bba8ab38eedf9b01
Nbalsh <brandon.bn.nelson@gmail.com>
2016-09-17 17:14:28 -0700

    Updating and Making Login or Register

--- bd-hack-marketplace/public/scripts/login.js
+++ bd-hack-marketplace/public/scripts/login.js
@@ -1,14 +1,48 @@
 var Login = React.createClass({
   getInitialState: function() {
-    return {login: true};
+    return {login: true, username: '', password: ''};
   },
   componentDidMount: function() {
   },
+  handleUsernameChange: function(e) {
+    this.setState({username: e.target.value});
+  },
+  handlePasswordChange: function(e) {
+    this.setState({password: e.target.value});
+  },
   handleClick: function() {
-      this.setState({login: !this.state.login});
+    this.setState({login: !this.state.login});
+  },
+  handleSubmitClick: function() {
+      if(this.state.login) { 
+        $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        cache: false,
+        success: function(data) {
+            //this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+            console.error(this.props.url, status, err.toString());
+        }.bind(this)
+        });
+      } else {
+          $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'POST',
+            data: comment,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                this.setState({data: comments});
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+            });
+      }
   },
   render: function() {
-    console.log(this.state.data);
     return (
       <div className="LoginStuffs">
         <div className="mainLogin">
@@ -16,10 +50,12 @@ var Login = React.createClass({
             <h1>{this.state.login ? 'Login to Your Account' : 'Create Your Account'}</h1>
             <br/>
             <form>
-              <input type="text" name="user" placeholder="Username"/>
-              <input type="password" name="pass" placeholder="Password"/>
-              <input type="submit" name="login" className="login loginmodal-submit" 
-                value={this.state.login ? 'Login' : 'Create Account'}/>
+              <input type="text" name="user" placeholder="Username" value={this.state.username}
+                onChange={this.handleUsernameChange}/>
+              <input type="password" name="pass" placeholder="Password" value={this.state.password}
+                onChange={this.handlePasswordChange}/>
+              <input type="button" name="login" className="login loginmodal-submit" 
+                value={this.state.login ? 'Login' : 'Create Account'} onClick={this.handleSubmitClick}/>
             </form>
             <div className="login-help">
               <a style={{cursor:'pointer'}} onClick={this.handleClick}>
@@ -36,6 +72,6 @@ var Login = React.createClass({
 });
 
 ReactDOM.render(
-  <Login />,
+  <Login url="/api/design"/>,
   document.getElementById('login')
 );
